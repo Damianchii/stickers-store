afc09a1a60f4deade3de31b202cdbeaa297103cb
Damianchii <damianczerwinsky@gmail.com>
2024-07-02 00:02:14 +0200

    end offer

--- frontend/src/Components/Offers/Offers.jsx
+++ frontend/src/Components/Offers/Offers.jsx
@@ -3,16 +3,16 @@ import exclusive_image from '../../Assets/exclusive_image.png'
 
 const Offers = () => {
 	return (
-		<div>
-			{/*offers*/}
-			<div>
-				{/*offers left*/}
-				<h1>Exclusive</h1>
-				<h1>Offers For You</h1>
-				<p>ONLY ON BEST SELLERS PRODUCTS</p>
-				<button>Check Now</button>
+		<div className='w-[65%] h-[60vh] flex m-auto px-[140px] mb-[150px] bg-hero-bg-radient'>
+			<div className='flex flex-1 flex-col justify-center'>
+				<h1 className='text-#171717 text-[80px] font-[600]'>Exclusive</h1>
+				<h1 className='text-#171717 text-[80px] font-[600]'>Offers For You</h1>
+				<p className='text-[#171717] text-[22px] font-[600]'>ONLY ON BEST SELLERS PRODUCTS</p>
+				<button className='w-[282px] bg-[#ff4141] text-[22px] h-[70px] rounded-full border-none text-white font-[500] mt-[30px] cursor-pointer'>
+					Check Now
+				</button>
 			</div>
-			<div>
+			<div className='flex-1 flex items-center justify-items-end pt-[50px]'>
 				{/*offers right*/}
 				<img src={exclusive_image} alt='exclusive image' />
 			</div>
